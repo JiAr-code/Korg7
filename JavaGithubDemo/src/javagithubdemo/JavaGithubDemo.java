b75f1900224041e50aa0d4b1195c338130659e5c
Jiyan <arogithub@gmail.com>
2021-04-10 11:39:35 +0200

    detta e en commit.

---
+++ JavaGithubDemo/src/javagithubdemo/JavaGithubDemo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagithubdemo;
+
+/**
+ *
+ * @author Jiyan Saron Rany Manne Samer Victoria Carolina Fatimeh Oday Michael
+ */
+public class JavaGithubDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Detta är ett test");
+    }
+    
+}
